2e8f64f844d55d671869e78aeafab96738b800a6
MAldeep <m7mdeldeeeep@gmail.com>
2026-09-01 21:54:15 +0300

    Edit : invalidation for patient query after every new patient added (tanstack cache magic)

--- app/hooks/patients/usePatients.ts
+++ app/hooks/patients/usePatients.ts
@@ -1,10 +1,11 @@
 import { patientServices } from "@/app/services/patientServices";
 import { useAuthStore } from "@/app/stores/useAuthStore";
 import { CreatePatientData } from "@/app/validations/patientValidation";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 export const usePatients = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   const user = useAuthStore((state) => state.user);
   const userRole = user?.role;
@@ -12,6 +13,7 @@ export const usePatients = () => {
     mutationFn: (patientData: CreatePatientData) =>
       patientServices.create(patientData),
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["patients"] });
       router.replace(`/dashboard/${userRole}/`);
     },
   });
